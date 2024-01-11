f969a7e242a35c55fc036586e93248a387478e78
Dennis Ochs <dennisochs17@gmail.com>
2024-01-11 23:11:38 +0100

    New updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,16 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { MainPageComponent } from "./main-page/main-page.component";
 import { AboutMeComponent } from "./about-me/about-me.component";
-
+import { MySkillsComponent } from "./my-skills/my-skills.component";
+import { PortfolioComponent } from "./portfolio/portfolio.component";
+import { ProjectJoinComponent } from "./project-join/project-join.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, HeaderComponent, MainPageComponent, AboutMeComponent]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, MainPageComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, ProjectJoinComponent]
 })
 export class AppComponent {
   title = 'my-portfolio';
---
+++ src/app/project-join/project-join.component.ts
@@ -0,0 +1,65 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-project-join',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './project-join.component.html',
+  styleUrl: './project-join.component.scss'
+})
+export class ProjectJoinComponent {
+  
+  state = 'normal';
+  isVisible = false;
+
+  @ViewChild('aboutmeLeft', { static: true }) aboutmeLeft: ElementRef | undefined;
+
+  
+  
+  hoveredStates: boolean[] = [];
+
+  showDescription(index: number) {
+    this.hoveredStates[index] = true;
+  }
+
+  hideDescription(index: number) {
+    this.hoveredStates[index] = false;
+  }
+
+  isDescriptionVisible(index: number): boolean {
+    return this.hoveredStates[index] || false;
+  }
+
+  ngAfterViewInit() {
+    this.observeVisibility();
+  }
+
+  observeVisibility() {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5,
+    };
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.isVisible = true; 
+          if (this.state !== 'unnormal') {
+            this.state = 'unnormal';
+          }
+        } else {
+          if (!this.isVisible) {
+            this.state = 'normal'; 
+          }
+        }
+      });
+    }, options);
+
+    if (this.aboutmeLeft) {
+      observer.observe(this.aboutmeLeft.nativeElement);
+    }
+  }
+}
+
